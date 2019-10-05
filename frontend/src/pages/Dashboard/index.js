fd0452809cf092cd80b1d5024e0ded4c2faa1603
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-10-05 00:35:40 -0300

    OmniStack 9.0 - Aula 02/10: Criada listagem de Spots

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 export default function Dashboard() {
-    return <h1>Dashboard</h1>;
+    const [ spots, setSpots ] = useState([]);
+    // Executa a função cada vez que uma das variávels do array é alterada
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+        }
+
+        loadSpots();
+    }, []); // Sem parâmetros: executa apenas uma vez, ao carregar o componente
+
+    return (
+        <>
+            <ul clasName="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
