2895ace4738d40bf3e6d87c016f6ca0e040678ad
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-10-05 00:47:05 -0300

    OmniStack 9.0 - Aula 02/10: Adicionado um campo virtual para a url da Thumbnail no backend

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -11,6 +11,15 @@ const SpotSchema = new mongoose.Schema({
         // Referencia o model 'User'
         ref: 'User'
     }
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+// Cria um novo campo que não existe no banco, é computado pelo JavaScript
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 /*
  * Importa o arquivo que gerencia as rotas. É necessário utilizar o caminho 
  * relativo (./), se não a aplicação busca por uma dependência externa
@@ -21,6 +22,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-qkjut.mongodb.net/
 app.use(cors());
 // Define que o express deve utilizar o formato JSON
 app.use(express.json());
+// Forma utilizada pelo express para retornar arquivos estáticos (pdf, imagem)
+app.use('/files', express.static(path.resolve(__dirname, '..', 'upload')));
 
 app.use(routes);
 
